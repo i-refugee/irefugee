5ba8f2a6b1eb6a2dc931329ace0de122994b1f0d
i-refugee <info@feelbit.com>
2016-03-28 18:36:07 +0300

    Update refugee.js

--- app/authenticators/refugee.js
+++ app/authenticators/refugee.js
@@ -1,6 +1,6 @@
 import OAuth2PasswordGrant from 'ember-simple-auth/authenticators/oauth2-password-grant';
 
 export default OAuth2PasswordGrant.extend({
-	serverTokenEndpoint: 'https://server.irefugee.gr/sessions',
-	serverTokenRevocationEndpoint: 'https://server.irefugee.gr/sessions/revoke'
+	serverTokenEndpoint: '',
+	serverTokenRevocationEndpoint: ''
 });
