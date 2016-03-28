48f6b7778eb7eaf3b50a99533c51eae9f3a7e914
i-refugee <info@feelbit.com>
2016-03-28 18:35:35 +0300

    Update application.js

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,7 +3,7 @@ import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
 	authorizer: 'authorizer:refugee',
-	host: 'https://server.irefugee.gr',
+	host: '',
 	pathForType: function(type) {
 		let underscored = Ember.String.underscore(type);
 		return Ember.String.pluralize(underscored);
